268c85302d852ab518f4e77eb8923a69b900d257
opera443399 <pc@pcswo.com>
2017-03-21 15:20:06 +0800

    typo

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.schemas import get_schema_view
 from rest_framework.routers import DefaultRouter
-from snippets import views as snippets_vies
+from snippets import views as snippets_views
 
 # Provide schema
 schema_view = get_schema_view(title='YOUR RESTFUL API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'snippets', snippets_vies.SnippetViewSet)
-router.register(r'users', snippets_vies.UserViewSet)
+router.register(r'snippets', snippets_views.SnippetViewSet)
+router.register(r'users', snippets_views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
